e6ffce09881cfaca89ed1975bc6f95c8f64125e2
Hammam-iris <m.hammam@iris-adv.com>
2018-06-19 14:29:48 +0200

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'M_Hammam:01229367833Mh*@ds042527.mlab.com:42527/m_hammam' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
