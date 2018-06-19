09aecdda339ce9f624b4c9c273e7f96040ce2339
Hammam-iris <m.hammam@iris-adv.com>
2018-06-19 14:43:24 +0200

    first commit

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '1739975719426934', // your App ID
 		'clientSecret' 	: '21c3de196beaa795915ec061574a3b70', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+		'callbackURL' 	: 'https://nodeauthentication.azurewebsites.net/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
 		'consumerKey' 		: 'J64ymbnjw0QND8OW6gmeq0CeU',
 		'consumerSecret' 	: 'cvjG76ut6qbneD6bFSbNkCsNzqLVqtTqvkVztL3ARtjjT8sbDD',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+		'callbackURL' 		: 'https://nodeauthentication.azurewebsites.net/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
 		'clientID' 		: '173587755816-h9hmho9svan9tnkeoj1iqfjb7fqmbo95.apps.googleusercontent.com',
 		'clientSecret' 	: 'e4oqtajdBx4KUlo65-WIWW4x',
-		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+		'callbackURL' 	: 'https://nodeauthentication.azurewebsites.net/auth/google/callback'
 	}
 
 };
